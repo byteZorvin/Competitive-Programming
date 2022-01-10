6d63d355d851123df8d15f922e39977928f6a676
Mehul0011 <75808688+Mehul0011@users.noreply.github.com>
2022-01-10 16:40:34 +0530

    Time: 4 ms (61.32%), Space: 6.4 MB (41.06%) - LeetHub

--- 67-add-binary/67-add-binary.cpp
+++ 67-add-binary/67-add-binary.cpp
@@ -3,7 +3,7 @@ public:
     string addBinary(string a, string b) {
         string ans = "";
         int i = a.size()-1, j = b.size()-1, carry = 0;
-        while(i>=0 or j>=0) {
+        while(i>=0 or j>=0 or carry) {
             int sum = carry;
             if(i>=0) sum += a[i--] - '0';
             if(j>=0) sum += b[j--] - '0';
@@ -11,8 +11,6 @@ public:
             ans += sum_final + '0';
             carry = (sum-sum_final)/2;
         }
-        if(carry != 0)
-            ans += carry + '0';
         reverse(ans.begin(), ans.end());
         return ans;
     }
