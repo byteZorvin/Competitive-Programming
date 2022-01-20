64f8c3e357a134e73f057ce8e552e7aa21f5b149
Mehul0011 <75808688+Mehul0011@users.noreply.github.com>
2022-01-20 16:55:56 +0530

    Time: 48 ms (97.35%), Space: 48.3 MB (49.78%) - LeetHub

--- 55-jump-game/55-jump-game.cpp
+++ 55-jump-game/55-jump-game.cpp
@@ -1,19 +1,11 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        int can_reach = 0, myPos = 0;
-        
-        while(can_reach < nums.size()-1) {
-            int range = can_reach;
-            for(int i = myPos; i<=can_reach; i++) {
-                if(i+nums[i] > range) {
-                    range = i + nums[i]; 
-                    myPos = i;
-                }
-            }
-            if(range == can_reach) return false;
-            can_reach = range;
+        int can_reach = 0, n = nums.size();
+        for(int i = 0; i<= can_reach; ++i) {
+            if(i == n-1) return true;
+            can_reach = max(can_reach, i+nums[i]);
         }
-        return can_reach >= nums.size() - 1;
+        return false;
     }
 };
