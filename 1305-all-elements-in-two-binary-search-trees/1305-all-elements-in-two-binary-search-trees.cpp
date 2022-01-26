25b89d1a98d38db0695cf0b93c39f6e5709cc7fc
Mehul0011 <75808688+Mehul0011@users.noreply.github.com>
2022-01-27 01:17:15 +0530

    Time: 201 ms (39.37%), Space: 84.2 MB (97.21%) - LeetHub

--- 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.cpp
+++ 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.cpp
@@ -10,50 +10,23 @@
  * };
  */
 class Solution {
+private:
+    void pushLeft(stack<TreeNode*> &s, TreeNode *n) {
+        while(n != NULL) 
+            s.push(exchange(n, n->left));
+    }
 public:
     vector<int> getAllElements(TreeNode* root1, TreeNode* root2) {
-        vector<int> ans;
-        
+        vector<int> ans;  
         stack<TreeNode*> st1, st2;
-        TreeNode* first = root1, *second = root2;
-
-        while(!st1.empty() or !st2.empty() or first or second) {
-            while(first != NULL) {
-                st1.push(first);
-                first = first->left;
-            }
-            while(second != NULL) {
-                st2.push(second);
-                second = second->left;
-            }
-            
-            if(!st1.empty() && !st2.empty()) {
-                if(st1.top()->val < st2.top()->val) {
-                    first = st1.top();
-                    st1.pop();
-                    ans.push_back(first->val);
-                    first = first->right;
-                } 
-                else{
-                    second = st2.top();
-                    st2.pop();
-                    ans.push_back(second->val);
-                    second = second->right;
-                }
-            }   
-            else if(!st1.empty()) {
-                first = st1.top();
-                st1.pop();
-                ans.push_back(first->val), first = first->right;
-            }
-            else {
-                second = st2.top();
-                st2.pop();
-                ans.push_back(second->val);
-                second = second->right;
-            }
+        pushLeft(st1, root1);
+        pushLeft(st2, root2);
+        while(!st1.empty() || !st2.empty()) {
+            stack<TreeNode*> &s = st1.empty() ? st2 : st2.empty() ? st1 : st1.top()->val <= st2.top()->val ? st1 : st2;
+            auto n = s.top(); s.pop();
+            ans.push_back(n->val);
+            pushLeft(s, n->right); 
         }
-        
         return ans;
     }
 };
