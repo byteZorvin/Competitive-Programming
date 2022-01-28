3d0e93fc64053bf4a5755c57914b7376262aab1c
Mehul0011 <75808688+Mehul0011@users.noreply.github.com>
2022-01-28 22:28:56 +0530

    Time: 446 ms (41.84%), Space: 65.3 MB (82.84%) - LeetHub

--- 421-maximum-xor-of-two-numbers-in-an-array/421-maximum-xor-of-two-numbers-in-an-array.cpp
+++ 421-maximum-xor-of-two-numbers-in-an-array/421-maximum-xor-of-two-numbers-in-an-array.cpp
@@ -1,52 +1,62 @@
+class Node {
+public:
+    Node* links[2];
+    Node() {
+        links[0] = links[1] =  NULL;
+    }
+    bool containsKey(int bit) {
+        return links[bit] != NULL;
+    }
+    Node* get(int bit) {
+        return links[bit];
+    }
+    void put(int bit, Node * node) {
+        links[bit] = node;
+    }
+};
+
+class Trie {
+private:
+    Node *root;
+public:   
+    Trie() {
+        root = new Node();
+    }
+public:
+    void insert(int num) {
+        Node* node = root;
+        for(int i = 31; i>=0; i--) {
+            int bit = (num >> i) & 1;
+            if(!node->containsKey(bit)) {
+                node->put(bit, new Node());
+            }
+            node = node->get(bit);
+        }
+    }
+public:
+    int getMax(int num) {
+        Node* node = root;
+        int maxNum = 0;
+        for(int i = 31; i>=0; i--) {
+            int bit = (num >> i) & 1;
+            if(node->containsKey(1 - bit)) {
+                maxNum |= 1 << i;
+                node = node->get(1 - bit);
+            }
+            else
+                node = node->get(bit);
+        }
+        return maxNum;
+    }
+};
+
 class Solution {
-private:
-    class Node {
-        public:
-        Node* child[2];
-        Node() {
-            child[0] = child[1] = NULL;
-        }
-    };
-private:
-    void insert(Node* head, int x) {
-        for(int i = 31; i>=0; i--) {
-            int bit = (x&(1<<i)) != 0 ? 1 : 0;
-            if(head->child[bit] == NULL) {
-                head->child[bit] = new Node; 
-            }
-            head = head->child[bit];
-        }
-        
-    }
-    
-private:
-    int max_xor(Node* head, int num) {
-        int ans = 0;
-        for(int i = 31; i>=0; i--) {
-            int bit = (num&(1<<i)) != 0 ? 0 : 1;
-            if(head->child[bit] != NULL) {
-                ans = (ans<<1) + 1;
-                head = head->child[bit];
-            }
-            else {
-                ans = (ans<<1) + 0;
-                head = head->child[!bit] + 0;
-            }
-        }
-        return ans;
-    }
-    
 public:
     int findMaximumXOR(vector<int>& nums) {
-        int n = nums.size();
-        Node * head = new Node;
-        for(int i = 0; i<n; i++) {
-            insert(head, nums[i]);
-        }
-        int ans = 0;
-        for(int i = 0; i<n; i++) {
-            ans = max(ans, max_xor(head, nums[i]));
-        }
-        return ans;
+        Trie trie;
+        int maxXor = 0;
+        for(auto &it: nums) trie.insert(it);
+        for(auto &it: nums) maxXor = max(maxXor, trie.getMax(it));
+        return maxXor;
     }
 };
