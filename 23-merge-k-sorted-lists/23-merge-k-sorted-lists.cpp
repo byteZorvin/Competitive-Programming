e0f78dfa195a938bd2b4fbae63cefa2cf6d03fec
Mehul0011 <75808688+Mehul0011@users.noreply.github.com>
2022-02-07 02:54:57 +0530

    Time: 16 ms (98.45%), Space: 13.3 MB (60.57%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.cpp
@@ -8,31 +8,49 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-class comp {
-public:
-    bool operator() (const ListNode* left, const ListNode* right) {
-        return left->val > right->val;
-    }
-};
-
-
-class Solution {    
-public:
-    ListNode* mergeKLists(vector<ListNode*>& lists) {
-        priority_queue<ListNode*, vector<ListNode*>, comp> pq;
-        ListNode dummy;
-        ListNode* head = &dummy, *tail = &dummy;
-        for(auto &list: lists) {
-            if(list) pq.push(list);
-        }
-        
-        while(!pq.empty()) {
-            auto node = pq.top();
-            pq.pop();
-            if(node->next) pq.push(node->next);
-            tail->next = node;
-            tail = tail->next;
+class Solution { 
+private:
+    ListNode* merge(ListNode* first, ListNode* second) {
+        ListNode*head = new ListNode();
+        ListNode* result = head;
+        while(first || second) {
+            if(first == NULL) {
+                result->next = second;
+                break;
+            }
+            if(second == NULL) {
+                result->next = first;
+                break;
+            }
+            else if(first->val <= second->val) {
+                result->next = first;
+                first = first->next;
+                result = result->next;
+            }
+            else {
+                result->next = second;
+                second = second->next;
+                result = result->next;
+            }
         }
         return head->next;
+    }
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        if(lists.size() == 0) return NULL;
+        int i = 0, last = lists.size()-1;
+        int j = last;
+        while(last != 0) {
+            i = 0, j = last;
+            while(i<j) {
+                lists[i] = merge(lists[i], lists[j]);
+                i++, j--;
+                if(i>=j) {
+                    last = j;
+                    break;
+                }
+            }
+        }
+        return lists[0];
     }
 };
