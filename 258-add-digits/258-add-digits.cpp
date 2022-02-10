529e351fe0cb689c21c4463c64ab1580b15016d8
Mehul0011 <75808688+Mehul0011@users.noreply.github.com>
2022-02-10 11:05:51 +0530

    Time: 0 ms (100.00%), Space: 6 MB (25.60%) - LeetHub

--- 258-add-digits/258-add-digits.cpp
+++ 258-add-digits/258-add-digits.cpp
@@ -1,16 +1,8 @@
 class Solution {
-private:
-    int addHelper(string s) {
-        if(s.size() == 1) return (s[0] - '0');
-        
-        int sum = 0;
-        for(auto &c: s) sum += (c - '0');
-        
-        return addHelper(to_string(sum));
-    }
 public:
     int addDigits(int num) {
-        string s = to_string(num);
-        return addHelper(s);
+        if(num == 0) return 0;
+        if(num%9 == 0) return 9;
+        else return num%9;
     }
 };
