6ac637ee7a7981dc809a51fab729ab1b1f2cf61e
Mehul0011 <75808688+Mehul0011@users.noreply.github.com>
2022-02-23 12:50:03 +0530

    Time: 15 ms (14.73%), Space: 8.9 MB (23.69%) - LeetHub

--- 133-clone-graph/133-clone-graph.cpp
+++ 133-clone-graph/133-clone-graph.cpp
@@ -20,39 +20,18 @@ public:
 */
 
 class Solution {
+private:
+    unordered_map<Node*, Node*> copy;
 public:
     Node* cloneGraph(Node* node) {
         if(node == NULL) return NULL;
         
-        unordered_map<int, Node*> allNodes;
-        set<pair<int, int>> edges;
-        queue<Node*>q; 
-        
-        Node* head = new Node(node->val);
-        allNodes[head->val] = head;
-        
-        q.push(node);
-        
-        while(!q.empty()) {
-            Node* curr = q.front(); q.pop();
-            for(auto &i: curr->neighbors) {
-                if(allNodes.find(i->val) == allNodes.end()) {
-                    Node* temp = new Node(i->val);
-                    allNodes[curr->val]->neighbors.push_back(temp);
-                    temp->neighbors.push_back(allNodes[curr->val]);
-                    edges.insert({curr->val, temp->val});
-                    edges.insert({temp->val, curr->val});
-                    allNodes[i->val] = temp;
-                    q.push(i);
-                }
-                else if(edges.find({curr->val, i->val}) == edges.end()){
-                    allNodes[curr->val]->neighbors.push_back(allNodes[i->val]);
-                    allNodes[i->val]->neighbors.push_back(allNodes[curr->val]);
-                    edges.insert({i->val, curr->val});
-                    edges.insert({curr->val, i->val});
-                }
+        if(copy.find(node) == copy.end()) {
+            copy[node] = new Node(node->val);
+            for(auto neigh: node->neighbors) {
+                copy[node]->neighbors.push_back(cloneGraph(neigh));
             }
         }
-        return head;
+        return copy[node];
     }
 };
