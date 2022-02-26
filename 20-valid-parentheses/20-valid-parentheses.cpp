add65cc81f5796811f1b3128855f43dc9bed1e32
Mehul0011 <75808688+Mehul0011@users.noreply.github.com>
2022-02-27 02:01:13 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (51.26%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+    
+        for(auto &c: s) {
+            if(c == '[' || c == '{' || c == '(') {
+                st.push(c);
+            }
+            else if(st.empty()) return false;
+            else if(c == ']' && st.top() != '[') return false;
+            else if(c == ')' && st.top() != '(') return false;
+            else if(c == '}' && st.top() != '{') return false;
+            else st.pop();
+        }
+        return st.empty();
+    }
+};
