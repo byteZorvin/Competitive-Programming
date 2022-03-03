b220fbc3acd00590293ea4defcc9f72ffd1f87a7
Mehul Chauhan <75808688+Mehul0011@users.noreply.github.com>
2022-03-03 09:44:55 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (16.43%) - LeetHub

--- 413-arithmetic-slices/413-arithmetic-slices.cpp
+++ 413-arithmetic-slices/413-arithmetic-slices.cpp
@@ -2,18 +2,14 @@ class Solution {
 public:
     int numberOfArithmeticSlices(vector<int>& nums) {
         int n = nums.size();
-        int ans = 0, count = 1;
+        int ans = 0;
+        vector<int> dp(n, 0);
         for(int i = 2; i<n; i++) {
-            if(nums[i] - nums[i-1] == nums[i-1] - nums[i-2]) 
-                count++;
-            else {
-                int sbarrcnt = count*(count+1)/2 - count;
-                ans += sbarrcnt > 0 ? sbarrcnt : 0;
-                count = 1;
+            if(nums[i] - nums[i-1] == nums[i-1] - nums[i-2]) {
+                dp[i] = dp[i-1] + 1;
             }
+            ans += dp[i];
         }
-        int sbarrcnt = count*(count+1)/2 - count;
-        ans += sbarrcnt > 0 ? sbarrcnt : 0;
         return ans;
     }
 };
