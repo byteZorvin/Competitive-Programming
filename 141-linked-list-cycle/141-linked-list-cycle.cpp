0f68752a364857a3d0ede6bc1601046d3f56862e
Mehul Chauhan <75808688+Mehul0011@users.noreply.github.com>
2022-03-08 13:00:49 +0530

    Time: 8 ms (95.50%), Space: 8 MB (81.34%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.cpp
+++ 141-linked-list-cycle/141-linked-list-cycle.cpp
@@ -11,8 +11,8 @@ public:
     bool hasCycle(ListNode *head) {
         ListNode* slow = head, *fast = head;
         while(fast && fast->next) {
-            slow = slow->next;
             fast = fast->next->next;
+            slow = slow->next;
             if(slow == fast) 
                 return true;
         }
