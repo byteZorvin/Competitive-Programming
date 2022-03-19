0d29994f9b84b33d9d83681e99b72d61553f4dc9
Mehul Chauhan <75808688+Mehul0011@users.noreply.github.com>
2022-03-19 19:06:59 +0530

    Time: 214 ms (77.69%), Space: 86.5 MB (49.79%) - LeetHub

---
+++ 895-maximum-frequency-stack/895-maximum-frequency-stack.cpp
@@ -0,0 +1,28 @@
+class FreqStack {   
+    unordered_map<int, int> freq;
+    unordered_map<int, stack<int>> m;
+    int maxFreq = 0;
+public:
+    FreqStack() {
+        
+    }
+    
+    void push(int val) {
+        maxFreq = max(maxFreq, ++freq[val]);
+        m[freq[val]].push(val);
+    }
+    
+    int pop() {
+        int x = m[maxFreq].top();
+        m[freq[x]--].pop();
+        if(!m[maxFreq].size()) maxFreq--;
+        return x;
+    }
+};
+
+/**
+ * Your FreqStack object will be instantiated and called as such:
+ * FreqStack* obj = new FreqStack();
+ * obj->push(val);
+ * int param_2 = obj->pop();
+ */
