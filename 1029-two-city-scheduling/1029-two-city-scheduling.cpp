bf99ac138e1984965f3a9eae472cf0c63e2ba14f
Mehul Chauhan <75808688+Mehul0011@users.noreply.github.com>
2022-03-25 10:04:03 +0530

    Time: 7 ms (52.53%), Space: 7.8 MB (78.03%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.cpp
+++ 1029-two-city-scheduling/1029-two-city-scheduling.cpp
@@ -1,32 +1,15 @@
 class Solution {
-private:
-    static bool mycmp(vector<int>&a, vector<int>&b) {
-        return abs(a[0] - a[1]) > abs(b[0] - b[1]);
+    static bool compare(vector<int> &a, vector<int> &b) {
+        return a[0]-a[1] > b[0] - b[1];
     }
 public:
     int twoCitySchedCost(vector<vector<int>>& costs) {
-        int cityA = 0, cityB = 0, cst = 0, n = costs.size();
-        n /= 2;
-        sort(costs.begin(), costs.end(), mycmp);
-        
-        for(int i = 0; i<2*n; i++) {
-            if(cityA<n && cityB<n) {
-                if(costs[i][0] < costs[i][1]) {
-                    cityA++;
-                    cst += costs[i][0];
-                } 
-                else 
-                    cst += costs[i][1], cityB++;
-            }
-            else if(cityA == n) {
-                cityB++;
-                cst += costs[i][1];
-            }
-            else {
-                cityA++;
-                cst += costs[i][0];
-            }
+        sort(costs.begin(), costs.end(), compare);
+        int ans = 0, a = 0, b = 0, n = costs.size();
+        for(int i = 0; i<n; i++) {
+            if(i<n/2) ans += costs[i][1];
+            else ans += costs[i][0];
         }
-        return cst;
+        return ans;
     }
 };
