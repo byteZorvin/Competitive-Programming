ac64d9192ec12f8c8a583241a97234b01aa93b2d
Mehul Chauhan <75808688+Mehul0011@users.noreply.github.com>
2022-04-27 01:31:05 +0530

    Time: 108 ms (95.29%), Space: 10.2 MB (93.38%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
@@ -2,28 +2,26 @@ class Solution {
 public:
     int minCostConnectPoints(vector<vector<int>>& points) {
         int n = points.size();
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> heap;
+        vector<int> minDist(n, INT_MAX);
         vector<bool> mst(n, false);
-        heap.push({0, 0});
+        minDist[0] = 0;
         
         int remaining = 0, minCost = 0;
-        while(remaining<n) {
-            pair<int, int> topElement = heap.top();
-            heap.pop();
-            
-            int weight = topElement.first;
-            int currNode = topElement.second;
-            if(mst[currNode]) continue;
-            
+        while(remaining<n) {            
+            int currNode = -1;
+            for(int node = 0; node<n; node++) {
+                if (!mst[node] & (currNode == -1 || minDist[currNode]>minDist[node]))
+                    currNode = node;
+            }
             mst[currNode] = true;
-            minCost += weight;
+            minCost += minDist[currNode];
             remaining++;
             
             for(int nextNode = 0; nextNode < n; nextNode++) {
                 if(mst[nextNode]) continue;
                 int weight = abs(points[currNode][0] - points[nextNode][0]) +
                                 abs(points[currNode][1] - points[nextNode][1]);
-                heap.push({weight, nextNode});
+                minDist[nextNode] = min(minDist[nextNode], weight);
             }
         }
         return minCost;
