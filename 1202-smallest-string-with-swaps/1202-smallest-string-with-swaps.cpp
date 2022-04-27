15fa04640be8f091349dfc5a0e574cb7bad86380
Mehul Chauhan <75808688+Mehul0011@users.noreply.github.com>
2022-04-27 14:05:24 +0530

    Time: 151 ms (93.96%), Space: 50.9 MB (68.03%) - LeetHub

--- 1202-smallest-string-with-swaps/1202-smallest-string-with-swaps.cpp
+++ 1202-smallest-string-with-swaps/1202-smallest-string-with-swaps.cpp
@@ -1,43 +1,66 @@
-class Solution {
-    bool visited[100010] = {0};
-    vector<int>adj[100010];
-    void DFS(int v, string&s, vector<char> &ch, vector<int> &ind) {
-        visited[v] = true;
-        ind.push_back(v);
-        ch.push_back(s[v]);
-        
-        for(int &neigh: adj[v]) {
-            if(!visited[neigh]) 
-                DFS(neigh, s, ch, ind);
+class UnionFind {
+    vector<int> parent;
+    vector<int> rank;
+public:
+    UnionFind(int size) {
+        parent = vector<int>(size);
+        rank = vector<int>(size);
+        for(int i = 0; i<size; ++i)
+            parent[i] = i;
+    }
+    
+    int findParent(int node) {
+        if(parent[node] == node) return node;
+        return parent[node] = findParent(parent[node]);
+    }
+    
+    void join(int u, int v) {
+        int x = findParent(u), y = findParent(v);
+        if(x == y) return;
+        if(rank[x] < rank[y]) {
+            parent[x] = y;
+        }
+        else if(rank[y] < rank[x]) {
+            parent[y] = x;
+        }
+        else {
+            parent[x] = y;
+            rank[y]++;
         }
     }
+};
+
+class Solution {
 public:
     string smallestStringWithSwaps(string s, vector<vector<int>>& pairs) {
+        UnionFind uf(s.size());
         
-        // Build adjacency list/Graph
         for(vector<int>&edge: pairs) {
             int source = edge[0], destination = edge[1];
-            //Undirected edge
-            adj[source].push_back(destination);
-            adj[destination].push_back(source);
+            // join the characters into a component
+            uf.join(source, destination);
         }
         
-        for(int vertex = 0; vertex<s.size(); vertex++) {
-            if(!visited[vertex]) {
-                vector<char> characters;
-                vector<int> indices;
-                
-                DFS(vertex, s, characters, indices);
-                
-                // Sort both the vectors and build the part string
-                sort(characters.begin(), characters.end());
-                sort(indices.begin(), indices.end());
-                
-                for(int index = 0; index < indices.size(); index++) {
-                    s[indices[index]] = characters[index];
-                }
+        unordered_map<int, vector<int>> parentTocomponent;
+        // Get the indexes of the characters of the same component
+        for(int vertex = 0; vertex < s.size(); vertex++) {
+            int par = uf.findParent(vertex);
+            parentTocomponent[par].push_back(vertex);
+        }
+        
+        string ans(s.size(), ' ');
+        for(auto component: parentTocomponent) {
+            vector<char> characters;
+            for(int index: component.second) {
+                characters.push_back(s[index]);
+            }
+            
+            //Sort the characters
+            sort(characters.begin(), characters.end());
+            for(int i = 0; i<characters.size(); i++) {
+                ans[component.second[i]] = characters[i];
             }
         }
-        return s;
+        return ans;
     }
 };
