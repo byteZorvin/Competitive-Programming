0649ef6db397c22de251fbbb3b47643e3d9dbfdb
Mehul Chauhan <75808688+Mehul0011@users.noreply.github.com>
2022-04-27 10:22:06 +0530

    Time: 989 ms (31.79%), Space: 58.4 MB (46.25%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
@@ -1,29 +1,74 @@
+class UnionFind {
+public:
+    vector<int> rank;
+    vector<int> parent;
+    
+    UnionFind(int size) {
+        rank = vector<int>(size);
+        parent = vector<int>(size);
+        for(int i = 0; i<size; ++i) 
+            parent[i] = i;
+    }
+    
+    int findPar(int node) {
+        if(parent[node] == node) 
+            return node;
+        return parent[node] = findPar(parent[node]);
+    }
+    
+    bool join(int node1, int node2) {
+        int x = findPar(node1), y = findPar(node2);
+        if(x == y) return false;
+        if(rank[x] > rank[y]) {
+            parent[y] = x;
+        }
+        else if(rank[x] < rank[y]) {
+            parent[y] = x;
+        }
+        else {
+            parent[x] = y;
+            rank[y]++;
+        } 
+        return true;
+    }
+};
+
 class Solution {
+    struct Edge {
+        int src, dest, weight;
+        Edge(int src, int dest, int weight) : src(src), dest(dest), weight(weight) {}
+    };
+    
+    static bool myCmp(Edge &e1, Edge &e2) {
+        return e1.weight < e2.weight;
+    }
 public:
     int minCostConnectPoints(vector<vector<int>>& points) {
         int n = points.size();
-        vector<int> minDist(n, INT_MAX);
-        vector<bool> mst(n, false);
-        minDist[0] = 0;
-        
-        int remaining = 0, minCost = 0;
-        while(remaining<n) {            
-            int currNode = -1;
-            for(int node = 0; node<n; node++) {
-                if (!mst[node] & (currNode == -1 || minDist[currNode]>minDist[node]))
-                    currNode = node;
-            }
-            mst[currNode] = true;
-            minCost += minDist[currNode];
-            remaining++;
-            
-            for(int nextNode = 0; nextNode < n; nextNode++) {
-                if(mst[nextNode]) continue;
-                int weight = abs(points[currNode][0] - points[nextNode][0]) +
-                                abs(points[currNode][1] - points[nextNode][1]);
-                minDist[nextNode] = min(minDist[nextNode], weight);
+        vector<Edge> allEdges;
+        for(int i = 0; i<n; i++) {
+            for(int j = i+1; j<n; j++) {
+                int wt = abs(points[i][0] - points[j][0]) + 
+                             abs(points[i][1] - points[j][1]);
+                allEdges.push_back(Edge(i, j, wt));
             }
         }
-        return minCost;
+        
+        // Sort all Edges on the basis of their weights.
+        sort(allEdges.begin(), allEdges.end(), myCmp);
+        
+        UnionFind uf(n);
+        int mstCost = 0;
+        
+        for(int i = 0, size = 0; size<n-1, i<allEdges.size(); i++) {
+            Edge e = allEdges[i];
+            int u = e.src, v = e.dest;
+            if(uf.join(u, v)) {
+                mstCost += e.weight;
+                size++;
+            }
+        }
+        
+        return mstCost;
     }
 };
