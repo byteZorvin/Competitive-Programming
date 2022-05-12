37cb75a95292743f855da9d77f1d5bc9aae485fa
Mehul Chauhan <75808688+Mehul0011@users.noreply.github.com>
2022-05-12 18:29:14 +0530

    Time: 35 ms (45.01%), Space: 24 MB (81.62%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/238-product-of-array-except-self.cpp
@@ -1,39 +1,14 @@
 class Solution {
-    int findIndex(vector<int>&nums, int x) {
-        int ind = -1;
-        for(int i = 0; i<nums.size(); i++) {
-            if(nums[i] == x)
-                ind = i;
-        }
-        return ind;
-    }
-    
-    int getProduct(vector<int>&nums) {
-        int pro = 1;
-        for(int i = 0; i<nums.size(); i++) {
-            if(nums[i])
-                pro *= nums[i];
-        }
-        return pro;
-    }
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        int zeroCount = count(nums.begin(), nums.end(), 0);
-        vector<int> ans(nums.size());
-        if(zeroCount > 1) 
-            return ans;
-        
-        int index = findIndex(nums, 0);
-        int product = getProduct(nums);
-        
-        if(zeroCount == 1) {
-            ans[index] = product;
+        int prefix = 1, suffix = 1, n = nums.size();
+        vector<int> res(n, 1);
+        for(int i = 0; i<n; i++) {
+            res[i] *= prefix;
+            prefix *= nums[i];
+            res[n-1-i] *= suffix;
+            suffix *= nums[n-1-i];
         }
-        else {
-            for(int i = 0; i<nums.size(); i++) {
-                ans[i] = product/nums[i];
-            }
-        }
-        return ans;
+        return res;
     }
 };
