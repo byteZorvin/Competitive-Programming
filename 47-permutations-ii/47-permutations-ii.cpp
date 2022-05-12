5a0c9edc8e1f2f1f8c3f46a8b413ecf87175140b
Mehul Chauhan <75808688+Mehul0011@users.noreply.github.com>
2022-05-12 08:52:14 +0530

    Time: 7 ms (80.95%), Space: 8.7 MB (75.71%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.cpp
+++ 47-permutations-ii/47-permutations-ii.cpp
@@ -1,20 +1,30 @@
 class Solution {
-    void generate(vector<vector<int>>&res, vector<int> nums, int i, int n) {
-        if(i == n - 1) {
-            res.push_back(nums);
+    void generate(vector<vector<int>>&res, vector<int> &perm, int n, unordered_map<int, int> &count) {
+        if(perm.size() == n) {
+            res.push_back(perm);
             return;
         }
-        for(int k = i; k<n; k++ ) {
-            if(k > i && nums[i] == nums[k]) continue;
-            swap(nums[i], nums[k]);
-            generate(res, nums, i+1, n);
+        for( auto &e: count ) {
+            if(e.second == 0) continue;
+            perm.push_back(e.first);
+            e.second--;
+            generate(res, perm, n, count);
+            perm.pop_back();
+            e.second++;
         }
+    }
+    unordered_map<int, int> getHash(vector<int>&nums) {
+        unordered_map<int, int> mp;
+        for(auto &i: nums) 
+            mp[i]++;
+        return mp;
     }
 public:
     vector<vector<int>> permuteUnique(vector<int>& nums) {
         vector<vector<int>> res;
-        sort(nums.begin(), nums.end());
-        generate(res, nums, 0, nums.size());
+        vector<int> perm;
+        unordered_map<int, int> count = getHash(nums);
+        generate(res, perm, nums.size(), count);
         return res;
     }
 };
