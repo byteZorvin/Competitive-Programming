2e51f910199917925009206ae371078a984cdd87
Mehul Chauhan <75808688+Mehul0011@users.noreply.github.com>
2022-05-14 18:37:49 +0530

    Time: 116 ms (92.68%), Space: 39.8 MB (82.77%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.cpp
+++ 743-network-delay-time/743-network-delay-time.cpp
@@ -1,15 +1,4 @@
 class Solution {
-    void DFS(vector<vector<pair<int, int>>> &adj, int currNode, 
-             int time, vector<int>&signalRecieved)
-    {   
-        if(time >= signalRecieved[currNode])
-            return;
-        
-        signalRecieved[currNode] = time;
-        
-        for(auto &edge: adj[currNode])
-            DFS(adj, edge.second, edge.first + time, signalRecieved);  
-    }
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
         // Build the adjacency list
@@ -17,12 +6,30 @@ public:
         for(auto &time: times) 
             adj[time[0]].push_back({time[2], time[1]});
         
-        for(int i = 1; i<=n; i++) {
-            sort(adj[i].begin(), adj[i].end());
-        }
         
         vector<int> signalRecieved(n+1, 1e7);
-        DFS(adj, k, 0, signalRecieved);
+        signalRecieved[k] = 0;
+        
+        queue<int> q;
+        q.push(k);
+        
+        while(!q.empty()) {
+            int n = q.size();
+            for(int i = 0; i<n; i++) {
+                int currNode = q.front();
+                q.pop();
+                
+                int now = signalRecieved[currNode];
+                
+                for(auto &edge: adj[currNode]) {
+                    int neighbour = edge.second;
+                    int timeReq = edge.first;
+                    if(signalRecieved[neighbour] > now + timeReq) 
+                        signalRecieved[neighbour] = now + timeReq,
+                        q.push(neighbour);
+                }
+            }
+        }
         
         int answer = 0;
         for(int node = 1; node <= n; node++) 
