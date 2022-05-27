eec5f67c139854ad8f6bffa991351b785ec8e1c2
Mehul Chauhan <75808688+Mehul0011@users.noreply.github.com>
2022-05-27 17:33:57 +0530

    Time: 443 ms (38.00%), Space: 9.8 MB (74.85%) - LeetHub

--- 474-ones-and-zeroes/474-ones-and-zeroes.cpp
+++ 474-ones-and-zeroes/474-ones-and-zeroes.cpp
@@ -1,23 +1,19 @@
 class Solution {
-    vector<vector<vector<int>>>dp;
-    int findMaxSubset(vector<string>&strs, int m, int n, int i) {
-        if(i<0) return 0;
-        
-        if(dp[i][m][n] != -1) return dp[i][m][n];
-        
-        int zeroCount = count(strs[i].begin(), strs[i].end(), '0');
-        int oneCount = strs[i].size() - zeroCount;
-        
-        int include = 0, notInclude = 0;
-        if(m>=zeroCount && n>=oneCount) 
-            include = 1 + findMaxSubset(strs, m - zeroCount, n - oneCount, i - 1);
-        notInclude = findMaxSubset(strs, m, n, i - 1);
-        
-        return dp[i][m][n] = max(include, notInclude);
-    }
 public:
     int findMaxForm(vector<string>& strs, int m, int n) {
-        dp.resize(strs.size()+1, vector<vector<int>>(m+1, vector<int>(n+1, -1)));
-        return findMaxSubset(strs, m, n, strs.size() - 1);
+        vector<vector<int>> dp(m+1, vector<int>(n+1, 0));
+        
+        for(auto &str: strs) {
+            int zeroCount = count(str.begin(), str.end(), '0');
+            int oneCount = str.size() - zeroCount;
+            
+            for(int i = m; i>=zeroCount; i--) {
+                for(int j = n; j>=oneCount; j--) {
+                    dp[i][j] = max(dp[i][j], 1 + dp[i-zeroCount][j-oneCount]);
+                }
+            }
+        }
+        
+        return dp[m][n];
     }
 };
