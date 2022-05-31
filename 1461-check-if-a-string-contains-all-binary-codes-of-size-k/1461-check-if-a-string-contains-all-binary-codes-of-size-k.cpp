da97da4e3bacc2320da7543822011370cba53e30
Mehul Chauhan <75808688+Mehul0011@users.noreply.github.com>
2022-05-31 21:25:30 +0530

    Time: 39 ms (99.06%), Space: 17.5 MB (96.86%) - LeetHub

--- 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.cpp
+++ 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.cpp
@@ -4,14 +4,17 @@ public:
         int n = s.size();
         if(n<(1<<k)+k-1) return false;
         
-        int found = 0;
-        vector<bool> visited(1<<k, false);
-    
-        for(int i = 0; i<n-k+1; i++) {
-            int curr = stoi(s.substr(i, k), nullptr, 2);
-            if(visited[curr] == false)
-                found++, visited[curr] = true;
-            if(found == 1<<k) return true;
+        int need = 1<<k;
+        int allOne = need-1;
+        int hashValue = 0;
+        vector<bool>found(need, false);
+        
+        for(int i = 0; i<n; i++) {
+            hashValue = ((hashValue<<1) & allOne | s[i] - '0');
+            if(!found[hashValue])
+                need--, found[hashValue] = true;
+            if(need == 0)
+                return true;
         }
         return false;
     }
