66fac439907f5be2783c6e3ce03245a2cb6eb993
Mehul Chauhan <75808688+Mehul0011@users.noreply.github.com>
2022-06-04 09:44:53 +0530

    Time: 10 ms (55.45%), Space: 7.2 MB (86.66%) - LeetHub

--- 51-n-queens/51-n-queens.cpp
+++ 51-n-queens/51-n-queens.cpp
@@ -1,36 +1,50 @@
 class Solution {
-private:
-    bool isSafe(int row, int col, vector<string>&board) {
-        for(int i = 0; i<col; i++) {
-            if(board[row][i] == 'Q') return false;
+    bool validCol(vector<string>&board, int currCol, int n) {
+        for(int i = 0; i<n; i++)
+            if(board[i][currCol] == 'Q') 
+                return false;
+        return true;
+    }
+    
+    bool validDiag(vector<string>&board, int currRow, int currCol, int n) {
+        int i = currRow, j = currCol;
+        while(i>=0 && j>=0) {
+            if(board[i][j] == 'Q')
+                return false;
+            i--, j--;
         }
-        for(int i = row, j = col; i<board.size() && j>=0; i++, j--) {
-            if(board[i][j] == 'Q') return false;
-        }
-        for(int i = row, j = col; i>=0 && j>=0; i--, j--) {
-            if(board[i][j] == 'Q') return false;
+        i = currRow, j = currCol;
+        while(i>=0 && j<n) {
+            if(board[i][j] == 'Q')
+                return false;
+            i--, j++;
         }
         return true;
     }
-private:
-    void solveUtil(int col, vector<vector<string>> &sol, vector<string> &board) {
-        if(col==board.size()) {
-            sol.push_back(board);
+    
+    bool Valid(vector<string>&board, int currRow, int currCol, int n) {
+        return validCol(board, currCol, n) && validDiag(board, currRow, currCol, n);
+    }
+    
+    void solve(vector<string>&board, int currRow, int n, vector<vector<string>>&solutions) {
+        if(currRow == n) {
+            solutions.push_back(board);
             return;
         }
-        for(int i = 0; i<board.size(); i++) {
-            if(isSafe(i, col, board)) {
-                board[i][col] = 'Q';
-                (solveUtil(col+1, sol, board));
-                board[i][col] = '.';
+        for(int i = 0; i<n; i++) {
+            if(Valid(board, currRow, i, n)) {
+                board[currRow][i] = 'Q';
+                solve(board, currRow+1, n, solutions);
+                board[currRow][i] = '.';
             }
         }
     }
+
 public:
     vector<vector<string>> solveNQueens(int n) {
-        vector<vector<string>> sol;
-        vector<string> board(n, string(n, '.'));
-        solveUtil(0, sol, board);
-        return sol;
+        vector<vector<string>>solutions;
+        vector<string>board(n, string(n, '.'));
+        solve(board, 0, n, solutions);
+        return solutions;
     }
 };
