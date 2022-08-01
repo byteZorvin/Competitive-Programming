2852d62715a77759cc15197e24a2a5162a4f413b
Mehul Chauhan <75808688+Mehul0011@users.noreply.github.com>
2022-08-01 12:55:32 +0530

    Time: 35 ms (37.51%), Space: 19.6 MB (59.41%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
-    int majorityElement(vector<int>& arr) {
-        int candidate = -1, votes = 0, n = arr.size(); 
-        for(int i = 0; i<n; i++) {
-            if(votes == 0) {
-                candidate = arr[i];
+    int majorityElement(vector<int>& nums) {
+        while(true) {
+            int candidate = nums[rand()%nums.size()];
+            int count = nums.size()/2;
+            for(auto n: nums) {
+                if(n == candidate) count--;
             }
-            votes += (candidate == arr[i]) ? 1: -1;
+            if(count < 0) return candidate;
         }
-        return candidate;
     }
 };
