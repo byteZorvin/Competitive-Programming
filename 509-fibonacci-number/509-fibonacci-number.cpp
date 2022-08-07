1d21bb56c9efd90ea37aa221cc035614d118d17f
Mehul Chauhan <75808688+Mehul0011@users.noreply.github.com>
2022-08-07 13:46:24 +0530

    Time: 5 ms (43.50%), Space: 5.9 MB (39.89%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,15 +1,13 @@
 class Solution {
-    int helper(int n, int *dp) {
-        if(dp[n] != -1) {
-            return dp[n];
-        }
-        if(n<=1) return dp[n] = n;
-        else return dp[n] = helper(n-1, dp) + helper(n-2, dp);
-    }
 public:
     int fib(int n) {
-        int dp[n+1];
-        memset(dp, -1, sizeof(dp));
-        return helper(n, dp);
+        if (n==0) return 0;
+        int a = 0, b = 1, c;
+        while(n-->1) {
+            c = a + b;
+            a = b;
+            b = c;
+        }
+        return b;
     }
 };
