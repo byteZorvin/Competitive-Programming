038a3521dc36c5013095104a16b23ae6407cb1a8
Mehul Chauhan <75808688+Mehul0011@users.noreply.github.com>
2022-08-17 01:02:29 +0530

    Time: 19 ms (20.86%), Space: 15.3 MB (18.49%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,20 +1,20 @@
 class Solution {
-    long long recursiveProfit(int ind, int buy, vector<int>&prices, vector<vector<long long>>&dp) {
-        if(ind == prices.size()) return 0;
-        int profit = 0;
-        if(dp[ind][buy] != -1) return dp[ind][buy];
-        if(buy) {
-            profit = max(-prices[ind] + recursiveProfit(ind+1, 0, prices, dp), recursiveProfit(ind+1, 1, prices, dp));
-        }
-        else {
-            profit = max(prices[ind] + recursiveProfit(ind+1, 1, prices, dp), recursiveProfit(ind+1, 0, prices, dp));
-        }
-        return dp[ind][buy] = profit;
-    }
 public:
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<vector<long long>>dp(n, vector<long long>(2, -1));
-        return recursiveProfit(0, 1, prices, dp);
+        vector<vector<long long>>dp(n+1, vector<long long>(2, 0));
+        for(int ind = n-1; ind>=0; ind--) {
+            for(int buy = 0; buy<=1; buy++) {
+                long long profit = 0;
+                if(buy) {
+                    profit = max(-prices[ind] + dp[ind+1][0], dp[ind+1][1]);
+                }
+                else {
+                    profit = max(prices[ind] + dp[ind+1][1], dp[ind+1][0]);
+                }
+                dp[ind][buy] = profit;
+            }
+        }
+        return dp[0][1];
     }
 };
