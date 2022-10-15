9b34345e631961e6491c9a02e7cb0d98aa98d843
Mehul Chauhan <75808688+Mehul0011@users.noreply.github.com>
2022-10-15 12:45:58 +0530

    Time: 1485 ms (5.01%), Space: 0B (100.00%) - LeetHub

--- 0178-rank-scores/0178-rank-scores.sql
+++ 0178-rank-scores/0178-rank-scores.sql
@@ -1,8 +1,8 @@
 # Write your MySQL query statement below
-select s1.score, 
-(
-    select count(distinct s2.score)
-    from scores s2
-    where s2.score >= s1.score
-) as 'rank'
-from scores s1 order by s1.score desc;
+
+SELECT S.score, count(distinct t.score) as 'rank'
+from scores s
+inner join
+scores t where s.score <= t.score
+group by s.id
+order by s.score desc;
