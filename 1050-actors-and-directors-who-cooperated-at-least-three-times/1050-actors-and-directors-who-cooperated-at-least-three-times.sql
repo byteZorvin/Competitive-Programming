1fe9622ecfc19bead913ea08890b82dc0a0921dc
Mehul Chauhan <75808688+Mehul0011@users.noreply.github.com>
2022-10-24 18:09:25 +0530

    Time: 930 ms (5.31%), Space: 0B (100.00%) - LeetHub

---
+++ 1050-actors-and-directors-who-cooperated-at-least-three-times/1050-actors-and-directors-who-cooperated-at-least-three-times.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+select
+    actor_id,
+    director_id
+from    
+    actorDirector ad
+group by actor_id, director_id
+having count(distinct ad.timestamp) >= 3;
