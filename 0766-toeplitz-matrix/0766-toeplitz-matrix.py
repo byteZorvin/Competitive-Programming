ae32d66941d015d6b64edc3077e6c86053d528ba
Mehul Chauhan <75808688+Mehul0011@users.noreply.github.com>
2022-10-31 08:59:02 +0530

    Time: 204 ms (32.93%), Space: 13.9 MB (36.74%) - LeetHub

--- 0766-toeplitz-matrix/0766-toeplitz-matrix.py
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.py
@@ -1,10 +1,6 @@
 class Solution:
     def isToeplitzMatrix(self, matrix: List[List[int]]) -> bool:
         groups = {}
-        for r, row in enumerate(matrix):
-            for c, val in enumerate(row):
-                if r-c not in groups:
-                    groups[r-c] = val
-                elif groups[r-c] != val:
-                    return False
-        return True
+        return all(r == 0 or c == 0 or matrix[r-1][c-1] == val 
+                  for r, row in enumerate(matrix)
+                  for c, val in enumerate(row))
