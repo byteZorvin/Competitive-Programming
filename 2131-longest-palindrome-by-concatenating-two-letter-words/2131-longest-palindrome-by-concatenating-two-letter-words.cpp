32396d929ac1d07e439041c1d54d8f7c9bb8bca6
Mehul Chauhan <75808688+Mehul0011@users.noreply.github.com>
2022-11-03 08:25:54 +0530

    Time: 650 ms (69.99%), Space: 168.4 MB (16.83%) - LeetHub

--- 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.cpp
+++ 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     int longestPalindrome(vector<string>& words) {
-        vector<vector<int>> m(26, vector<int>(26, 0));
+        vector<vector<int>> m(26, vector<int>(26));
         int ans = 0;
-        for(auto &w: words) {
+        for(auto w: words) {
             int a = w[0] - 'a', b = w[1] - 'a';
             if(m[b][a] > 0) m[b][a]--, ans += 4;
             else m[a][b]++;
