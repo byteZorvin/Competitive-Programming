04317c5b2818d85095aa82ef1cc4aec6119b3c99
Mehul Chauhan <75808688+Mehul0011@users.noreply.github.com>
2022-11-03 08:20:26 +0530

    Time: 1860 ms (77.90%), Space: 38.4 MB (54.38%) - LeetHub

---
+++ 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.py
@@ -0,0 +1,21 @@
+class Solution:
+    def longestPalindrome(self, words: List[str]) -> int:
+        m = defaultdict(int)
+        unpaired = ans = 0
+        for w in words:
+            if w[0] == w[1]: 
+                if m[w] > 0:
+                    m[w] -= 1
+                    unpaired -= 1
+                    ans += 4
+                else:
+                    m[w] += 1
+                    unpaired += 1
+            else: 
+                if m[w[::-1]] > 0: 
+                    ans += 4
+                    m[w[::-1]] -= 1
+                else: m[w] += 1
+        if unpaired > 0: ans += 2
+        return ans
+                        
