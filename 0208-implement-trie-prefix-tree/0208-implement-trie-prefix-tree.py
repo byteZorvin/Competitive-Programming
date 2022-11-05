f81aacda5a724fea19e939bd63721a92e773dd06
Mehul Chauhan <75808688+Mehul0011@users.noreply.github.com>
2022-11-05 12:20:08 +0530

    Time: 114 ms (70.46%), Space: 45.1 MB (51.41%) - LeetHub

--- 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.py
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.py
@@ -1,40 +1,68 @@
-class TrieNode: 
-    def __init__(self):
-        self.word = False
-        self.children = {}
+class TrieNode {
+public:
+    bool ends;
+    TrieNode* children[26];
+    
+    TrieNode() {
+        for(int i = 0; i<26; i++) {
+            children[i] = NULL;
+        }
+        ends = false;
+    }
+};
 
-class Trie:
+class Trie {
+    TrieNode *root;
+public:
+    Trie() {
+        root = new TrieNode();
+    }
+    
+    void insert(string word) {
+        int n = word.size();
+        TrieNode*cur = root;
+        for(int i = 0; i<n; i++) {
+            int c = word[i] - 'a';
+            if(cur->children[c] == NULL) {
+                TrieNode* newNode = new TrieNode();
+                cur->children[c] = newNode;
+            }
+            cur = cur->children[c];
+        }
+        cur->ends = true;
+    }
+    
+    bool search(string word) {
+        int i = 0; 
+        TrieNode* cur = root;
+        while(i<word.size()) {
+            int c = word[i] - 'a';
+            if(cur->children[c] == NULL){
+                return false;
+            }
+            cur = cur->children[c];
+            i++;
+        }
+        return cur->ends;
+    }
+    
+    bool startsWith(string word) {
+        int i = 0;
+        TrieNode* cur = root;
+        while(i<word.size()) {
+            int c = word[i] - 'a';
+            if(cur->children[c] == NULL) return false;
+            cur = cur->children[c];
+            i++;
+        }
+        return true;
+    }
+};
 
-    def __init__(self):
-        self.root = TrieNode()
-
-    def insert(self, word: str) -> None:
-        node = self.root
-        for i in word: 
-            if i not in node.children:
-                node.children[i] = TrieNode()
-            node = node.children[i]
-        node.word = True
-
-    def search(self, word: str) -> bool:
-        node = self.root
-        for i in word: 
-            if i not in node.children:
-                return False
-            node = node.children[i]
-        return node.word
-
-    def startsWith(self, word: str) -> bool:
-        node = self.root
-        for i in word: 
-            if i not in node.children:
-                return False
-            node = node.children[i]
-        return True
-
-
-# Your Trie object will be instantiated and called as such:
-# obj = Trie()
-# obj.insert(word)
-# param_2 = obj.search(word)
-# param_3 = obj.startsWith(prefix)
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie* obj = new Trie();
+ * obj->insert(word);
+ * bool param_2 = obj->search(word);
+ * bool param_3 = obj->startsWith(prefix);
+ */
