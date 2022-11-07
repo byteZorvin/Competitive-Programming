3b8f5750a89b3a3ddefd407d84a3169e8cb8381f
Mehul Chauhan <75808688+Mehul0011@users.noreply.github.com>
2022-11-07 08:38:45 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (45.85%) - LeetHub

--- 1323-maximum-69-number/1323-maximum-69-number.cpp
+++ 1323-maximum-69-number/1323-maximum-69-number.cpp
@@ -2,16 +2,14 @@ class Solution {
 public:
     int maximum69Number (int num) {
         string numString = to_string(num);
-        int sixIndex = -1, i = 0;
+        int i = 0;
         while(i<numString.size()) {
             if(numString[i] == '6') {
-                sixIndex = i;
+                numString[i] = '9';
                 break;
             }
             i++;
         }
-        if(sixIndex != -1)
-            numString[sixIndex] = '9';
         return stoi(numString);
     }
 };
