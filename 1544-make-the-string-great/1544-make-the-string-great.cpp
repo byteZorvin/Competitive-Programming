8f6bda99b4a0abc295ab6af6c2b2ae98487ea07f
Mehul Chauhan <75808688+Mehul0011@users.noreply.github.com>
2022-11-08 15:51:46 +0530

    Time: 4 ms (64.51%), Space: 6.2 MB (92.00%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.cpp
+++ 1544-make-the-string-great/1544-make-the-string-great.cpp
@@ -1,26 +1,14 @@
 class Solution {
-    string helper(string s) {
-        int n = s.size();
-        int i = 0;
-        
-        string ans;
-        for(i = 0; i<n-1; i++) {
-            if(tolower(s[i]) == tolower(s[i+1]) && s[i] != s[i+1])
-                i++;
-            else ans += s[i];
-        }
-        if(i == n-1) 
-            ans += s[i];
-        
-        return ans;
-    }
 public:
     string makeGood(string s) {
-        string ans = helper(s);
-        while(s != ans) {
-            s = ans; 
-            ans = helper(s);
+        int i = 0;
+        for(int j = 0; j<s.size(); j++) {
+            if(i>0 and abs(s[i-1]-s[j]) == 32) i--;
+            else {
+                s[i] = s[j];
+                i++;
+            }
         }
-        return ans;
+        return s.substr(0, i);
     }
 };
