ecdcc1ee7269035148052c9702bd6b86fc672a87
Mehul Chauhan <75808688+Mehul0011@users.noreply.github.com>
2022-11-15 00:37:13 +0530

    Time: 1419 ms (5.01%), Space: 384.8 MB (5.01%) - LeetHub

--- 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
@@ -1,20 +1,17 @@
 class Solution {
-    void dfs(vector<vector<int>> &adjRow, 
-             vector<vector<int>> &adjCol,
-             int x, int y, 
-             vector<vector<bool>>& visited
-            ) {
+    void dfs(vector<vector<int>> &adjRow, vector<vector<int>> &adjCol, int x, int y, 
+             vector<vector<bool>>& visited) {
         
         visited[x][y] = true;
-        for(int j = 0; j<adjRow[x].size(); j++) {
-            int y = adjRow[x][j];
-            if(!visited[x][y])
-                dfs(adjRow, adjCol, x, y, visited);
+        
+        for(auto j: adjRow[x]) {
+            if(!visited[x][j])  
+                dfs(adjRow, adjCol, x, j, visited);
         }
-        for(int i = 0; i<adjCol[y].size(); i++) {
-            int x = adjCol[y][i];
-            if(!visited[x][y]) 
-                dfs(adjRow, adjCol, x, y, visited);
+        
+        for(auto i: adjCol[y]) {
+            if(!visited[i][y])
+                dfs(adjRow, adjCol, i, y, visited);
         }
     }
 public:
@@ -28,13 +25,11 @@ public:
         }
         vector<vector<bool>> visited(10001, vector<bool>(10001, false));
         int count = 0;
-        for(int i = 0; i<adjRow.size(); i++) {
-            for(int j = 0; j<adjRow[i].size(); j++) {
-                int x = i, y = adjRow[i][j];
-                if(!visited[x][y]) {
-                    dfs(adjRow, adjCol, x, y, visited);
-                    count++;
-                }
+        for(int i=0; i<adjRow.size(); i++){
+            for(int j=0; j<adjRow[i].size(); j++){
+                
+                if(!visited[i][adjRow[i][j]]) 
+                    dfs(adjRow, adjCol, i, adjRow[i][j], visited), count++;
             }
         }
         return m - count;
